223933d9aeff3e2474921d87c08fbf58ccb909ae
Omidznlp <87664653+Omidznlp@users.noreply.github.com>
2021-07-26 18:29:16 +0200

    update setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 
 sudo pip3 install django==3.0.7
 sudo pip3 install pip install djangorestframework
+sudo pip3 install Pyroute2
 sudo chmod +x ./startup.sh
 sudo chmod +x ./kill.sh
 sudo chmod + x ./ospf/shell/*.*
